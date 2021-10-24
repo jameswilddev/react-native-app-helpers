335e7d75b5f61601527af2da5eaea364fc452024
jameswilddev <jameswilddev@outlook.com>
2021-10-24 23:51:59 +0100

    Add a state store.

---
+++ services/StateStore/index.tsx
@@ -0,0 +1,161 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { EventEmitter } from "events";
+import type { Json } from "../../types/Json";
+
+/**
+ * A wrapper around AsyncStorage which adds:
+ * - Concurrency control.
+ * - JSON parsing and serialization.
+ * - Change events.
+ * - A synchronous read/write API (with asynchronous write-back).
+ * @template T The type of JSON stored.
+ */
+export class StateStore<T extends Json> {
+  private asyncStorageKey: null | string = null;
+  private value: undefined | T = undefined;
+  private writeQueueLength: 0 | 1 | 2 = 0;
+  private resolveOnUnload: null | (() => void) = null;
+
+  private readonly eventEmitter = new EventEmitter();
+
+  /**
+   * @param initial The value to use when no such record exists in AsyncStorage.
+   */
+  constructor(private readonly initial: T) { }
+
+  /**
+   * Adds a listener for events to this state store.
+   * @param eventType The type of the event to listen for.
+   * @param listener  The callback to execute when the event is emitted.
+   */
+  addListener(eventType: `set`, listener: () => void): void {
+    this.eventEmitter.addListener(eventType, listener);
+  }
+
+  /**
+   * Removes a listener for events from this state store.
+   * @param eventType The type of the event to listen for.
+   * @param listener  The callback to no longer execute when the event is
+   *                  emitted.
+   */
+  removeListener(eventType: `set`, listener: () => void): void {
+    this.eventEmitter.removeListener(eventType, listener);
+  }
+
+  /**
+   * Loads the content of a record in AsyncStorage into memory.
+   * @param asyncStorageKey The key of the record to read from/write to
+   *                        AsyncStorage.
+   * @throws When the state store is already loading.
+   * @throws When the state store is already loaded.
+   * @throws When the state store is currently unloading.
+   */
+  async load(asyncStorageKey: string): Promise<void> {
+    if (this.resolveOnUnload !== null) {
+      throw new Error(`The state store is currently unloading.`);
+    } else if (this.value !== undefined) {
+      throw new Error(`The state store is already loaded.`);
+    } else if (this.asyncStorageKey !== null) {
+      throw new Error(`The state store is already loading.`);
+    } else {
+      this.asyncStorageKey = asyncStorageKey;
+
+      const raw = await AsyncStorage.getItem(asyncStorageKey);
+
+      if (raw === null) {
+        this.value = this.initial;
+      } else {
+        this.value = JSON.parse(raw);
+      }
+    }
+  }
+
+  /**
+   * Retrieves the current value from the store.
+   * @returns The store's current value.
+   * @throws When the state store is unloaded.
+   * @throws When the state store is currently loading.
+   * @throws When the state store is currently unloading.
+   */
+  get(): T {
+    if (this.resolveOnUnload !== null) {
+      throw new Error(`The state store is currently unloading.`);
+    } else if (this.asyncStorageKey === null) {
+      throw new Error(`The state store is not loaded.`);
+    } else if (this.value === undefined) {
+      throw new Error(`The state store is currently loading.`);
+    } else {
+      return this.value;
+    }
+  }
+
+  private startWrite(): void {
+    (async () => {
+      await AsyncStorage.setItem(
+        this.asyncStorageKey as string,
+        JSON.stringify(this.value as T)
+      );
+
+      this.writeQueueLength--;
+
+      if (this.writeQueueLength > 0) {
+        this.startWrite();
+      } else if (this.resolveOnUnload !== null) {
+        const resolveOnUnload = this.resolveOnUnload;
+        this.asyncStorageKey = null;
+        this.value = undefined;
+        this.resolveOnUnload = null;
+        resolveOnUnload();
+      }
+    })();
+  }
+
+  /**
+   * Sets the current value in the store.  It will be written back to disk
+   * asynchronously.
+   * @param to The value to which to change.
+   * @throws When the state store is unloaded.
+   * @throws When the state store is currently loading.
+   * @throws When the state store is currently unloading.
+   */
+  set(to: T): void {
+    if (this.resolveOnUnload !== null) {
+      throw new Error(`The state store is currently unloading.`);
+    } else if (this.asyncStorageKey === null) {
+      throw new Error(`The state store is not loaded.`);
+    } else if (this.value === undefined) {
+      throw new Error(`The state store is currently loading.`);
+    } else {
+      this.value = to;
+
+      if (this.writeQueueLength === 0) {
+        this.writeQueueLength = 1;
+        this.startWrite();
+      } else {
+        this.writeQueueLength = 2;
+      }
+
+      this.eventEmitter.emit(`set`);
+    }
+  }
+
+  /**
+   * Awaits any queued writes and unloads the current record from memory.
+   */
+  async unload(): Promise<void> {
+    if (this.resolveOnUnload !== null) {
+      throw new Error(`The state store is already unloading.`);
+    } else if (this.asyncStorageKey === null) {
+      throw new Error(`The state store is not loaded.`);
+    } else if (this.value === undefined) {
+      throw new Error(`The state store is currently loading.`);
+    } else if (this.writeQueueLength > 0) {
+      await new Promise<void>((resolve) => {
+        this.resolveOnUnload = resolve;
+      });
+    } else {
+      this.asyncStorageKey = null;
+      this.value = undefined;
+    }
+  }
+}
---
+++ services/StateStore/readme.md
@@ -0,0 +1,47 @@
+# `react-native-app-helpers/StateStore`
+
+A wrapper around AsyncStorage which adds:
+
+- Concurrency control.
+- JSON parsing and serialization.
+- Change events.
+- A synchronous read/write API (with asynchronous write-back).
+
+## Usage
+
+```tsx
+import type { StateStore } from "react-native-app-helpers";
+
+type State = `State A` | `State B`;
+
+const store = new StateStore<State>(`State A`);
+
+
+await store.load(`AsyncStorage Key A`);
+
+// State A
+console.log(store.get());
+
+store.set(`State B`);
+
+// State B
+console.log(store.get());
+
+await store.unload();
+
+
+await store.load(`AsyncStorage Key A`);
+
+// State A
+console.log(store.get());
+
+await store.unload();
+
+
+await store.load(`AsyncStorage Key A`);
+
+// State B
+console.log(store.get());
+
+await store.unload();
+```
