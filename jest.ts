d239ea921108938c96ddb2dccdb612b14ecf2437
jameswilddev <jameswilddev@outlook.com>
2021-10-25 08:23:55 +0100

    Workaround attempt.

--- jest.ts
+++ jest.ts
@@ -1,8 +1,17 @@
 (global as unknown as { __DEV__: boolean }).__DEV__ = false;
 
-jest.mock(`@react-native-async-storage/async-storage`, () =>
-  require(`@react-native-async-storage/async-storage/jest/async-storage-mock`)
-);
+jest.mock(`@react-native-async-storage/async-storage`, () => {
+  const asyncStorage = new Map<string, string>();
+
+  return {
+    async getItem(key: string): Promise<null | string> {
+      return asyncStorage.get(key) ?? null;
+    },
+    async setItem(key: string, value: string): Promise<void> {
+      asyncStorage.set(key, value);
+    },
+  };
+});
 
 jest.mock(`react-native-encrypted-storage`, () => {
   const encryptedStorage = new Map<string, string>();
