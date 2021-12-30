f76ddd663f2a46e1597d0bc941168fd0db89fb00
jameswilddev <jameswilddev@outlook.com>
2021-12-30 16:27:29 +0000

    Remove comments now added by interface.

--- react-native/services/StateStore/index.tsx
+++ react-native/services/StateStore/index.tsx
@@ -25,32 +25,14 @@ export class StateStore<T extends Json> implements StateStoreInterface<T> {
    */
   constructor(private readonly initial: T) {}
 
-  /**
-   * Adds a listener for events to this state store.
-   * @param eventType The type of the event to listen for.
-   * @param listener  The callback to execute when the event is emitted.
-   */
   addListener(eventType: `set`, listener: () => void): void {
     this.eventEmitter.addListener(eventType, listener);
   }
 
-  /**
-   * Removes a listener for events from this state store.
-   * @param eventType The type of the event to listen for.
-   * @param listener  The callback to no longer execute when the event is
-   *                  emitted.
-   */
   removeListener(eventType: `set`, listener: () => void): void {
     this.eventEmitter.removeListener(eventType, listener);
   }
 
-  /**
-   * Loads the content of a record in expo-file-system into memory.
-   * @param key The key of the record to read from/write to expo-file-store.
-   * @throws When the state store is already loading.
-   * @throws When the state store is already loaded.
-   * @throws When the state store is currently unloading.
-   */
   async load(key: string): Promise<void> {
     if (this.resolveOnUnload !== null) {
       throw new Error(`The state store is currently unloading.`);
@@ -77,13 +59,6 @@ export class StateStore<T extends Json> implements StateStoreInterface<T> {
     }
   }
 
-  /**
-   * Retrieves the current value from the store.
-   * @returns The store's current value.
-   * @throws When the state store is unloaded.
-   * @throws When the state store is currently loading.
-   * @throws When the state store is currently unloading.
-   */
   get(): T {
     if (this.resolveOnUnload !== null) {
       throw new Error(`The state store is currently unloading.`);
@@ -117,14 +92,6 @@ export class StateStore<T extends Json> implements StateStoreInterface<T> {
     })();
   }
 
-  /**
-   * Sets the current value in the store.  It will be written back to disk
-   * asynchronously.
-   * @param to The value to which to change.
-   * @throws When the state store is unloaded.
-   * @throws When the state store is currently loading.
-   * @throws When the state store is currently unloading.
-   */
   set(to: T): void {
     if (this.resolveOnUnload !== null) {
       throw new Error(`The state store is currently unloading.`);
@@ -146,9 +113,6 @@ export class StateStore<T extends Json> implements StateStoreInterface<T> {
     }
   }
 
-  /**
-   * Awaits any queued writes and unloads the current record from memory.
-   */
   async unload(): Promise<void> {
     if (this.resolveOnUnload !== null) {
       throw new Error(`The state store is already unloading.`);
