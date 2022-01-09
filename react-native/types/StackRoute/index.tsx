3e1a94120cbfe8129b434199ca852317d666b8b9
jameswilddev <jameswilddev@outlook.com>
2022-01-09 09:21:34 +0000

    Document.

--- react-native/types/StackRoute/index.tsx
+++ react-native/types/StackRoute/index.tsx
@@ -31,17 +31,50 @@ export type StackRoute<
   TOtherProps extends { readonly [key: string]: unknown }
 > = React.FunctionComponent<
   {
+    /**
+     * Call to add one or more card(s) to the top of the stack.
+     * @param itemsToAdd The card(s) to add.
+     */
     push(...itemsToAdd: ReadonlyArray<Item<TRouteParameters>>): void;
+
+    /**
+     * Pops one or more card(s) from the top of the stack.
+     * @param numberOfItemsToRemove The number of card(s) to remove.  Defaults
+     *                              to 1 when not given.
+     */
     pop(numberOfItemsToRemove?: number): void;
+
+    /**
+     * Pops one or more card(s) from the top of stack, then adds one or more
+     * card(s) to the top of the stack in their place.
+     * @param numberOfItemsToRemove The number of card(s) to remove.
+     * @param itemsToAdd            The card(s) to add in their place.
+     */
     replace(
       numberOfItemsToRemove: number,
       ...itemsToAdd: ReadonlyArray<Item<TRouteParameters>>
     ): void;
+
+    /**
+     * Resets the entire card stack to a new stack.
+     * @param replacementItems The replacement stack.
+     */
     reset(...replacementItems: ReadonlyArray<Item<TRouteParameters>>): void;
+
+    /**
+     * The route parameters for this card.
+     */
     readonly parameters: TRouteParameters[TRouteKey];
+
+    /**
+     * The state of the stack router.
+     */
     readonly routeState: StackRouterState<TRouteParameters>;
-    readonly setRouteState: (to: StackRouterState<TRouteParameters>) => void;
-    readonly width: `fillsContainer` | `fitsContent`;
-    readonly height: `fillsContainer` | `fitsContent`;
+
+    /**
+     * Sets a new state within the stack router.
+     * @param to The new state to set.
+     */
+    setRouteState(to: StackRouterState<TRouteParameters>): void;
   } & TOtherProps
 >;
